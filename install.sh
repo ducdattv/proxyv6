f8a5bd7ac605774c10efafe26e9c6f24c3b4647b
ducdattv <ducdattv@gmail.com>
2022-04-16 23:54:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,9 +12,10 @@ gen64() {
 	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 install_3proxy() {
-	URL="https://github.com/z3APA3A/3proxy/archive/0.8.13.tar.gz"
+    echo "installing 3proxy"
+    URL="https://github.com/ducdattv/proxyv6/raw/main/3proxy-3proxy-0.8.6.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-0.8.13
+    cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
@@ -26,20 +27,17 @@ install_3proxy() {
 
 gen_3proxy() {
     cat <<EOF
-#This file was automatically generated
 daemon
-log /var/log/3proxy/3proxy.log D
+maxconn 1000
 nscache 65536
-
-auth iponly
-allow * 188.165.205.32,46.105.101.74,144.217.64.10,158.69.126.183,144.217.183.123,14.248.83.36
-deny * *
-socks -a -p3128
+timeouts 1 5 30 60 180 1800 15 60
+setgid 65535
+setuid 65535
 flush
-
-$(awk -F "/" '{print "auth iponly\n" \
-"allow * 188.165.205.32,46.105.101.74,144.217.64.10,158.69.126.183,144.217.183.123,14.248.83.36\n" \
-"deny * * \n" \
+auth strong
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
+$(awk -F "/" '{print "auth strong\n" \
+"allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
@@ -51,15 +49,25 @@ $(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
+upload_proxy() {
+    local PASS=$(random)
+    zip --password $PASS proxy.zip proxy.txt
+    URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
+
+    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+    echo "Download zip archive from: ${URL}"
+    echo "Password: ${PASS}"
+
+}
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "avgdha/ddhaJzgx/$IP4/$port/$(gen64 $IP6)"
+        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
 gen_iptables() {
     cat <<EOF
-    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA})
+    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
 EOF
 }
 
@@ -68,6 +76,7 @@ gen_ifconfig() {
 $(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
+echo "installing apps"
 yum -y install gcc net-tools bsdtar zip >/dev/null
 
 install_3proxy
@@ -80,42 +89,30 @@ mkdir $WORKDIR && cd $_
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-COUNT=100
-FIRST_PORT=20000
+echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
+
+echo "How many proxy do you want to create? Example 500"
+read COUNT
+
+FIRST_PORT=10000
 LAST_PORT=$(($FIRST_PORT + $COUNT))
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
-chmod +x boot_*.sh /etc/rc.local
+chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
 cat >>/etc/rc.local <<EOF
-bash /home/proxy-installer/boot_iptables.sh
-bash /home/proxy-installer/boot_ifconfig.sh
-ulimit -n 200010
-systemctl start 3proxy.service
+bash ${WORKDIR}/boot_iptables.sh
+bash ${WORKDIR}/boot_ifconfig.sh
+ulimit -n 10048
+service 3proxy start
 EOF
 
 bash /etc/rc.local
 
 gen_proxy_file_for_user
 
-#upload_proxy
-
-mkdir /var/log/3proxy
-
-echo "* hard nofile 200010" >> /etc/security/limits.conf
-echo "* soft nofile 200010" >> /etc/security/limits.conf
-echo "root hard nofile 200010" >> /etc/security/limits.conf
-echo "root soft nofile 200010" >> /etc/security/limits.conf
-echo "* hard nproc 200010" >> /etc/security/limits.conf
-echo "* soft nproc 200010" >> /etc/security/limits.conf
-echo "root hard nproc 200010" >> /etc/security/limits.conf
-echo "root soft nproc 200010" >> /etc/security/limits.conf
-sysctl -w fs.file-max=400020
-sysctl -p
-sudo systemctl disable firewalld
-sudo systemctl mask --now firewalld
-reboot
+upload_proxy
